8b2377e59e356a517ab54b79a529d9d43bd7c63f
Matías Bonifazi <matiasbonifazi00@gmail.com>
2024-03-22 16:54:58 -0300

    Archivo-Segunda funcionalidad

--- game.py
+++ game.py
@@ -1,18 +1,24 @@
 import random
 # Lista de palabras posibles
 words = ["python", "programación", "computadora", "código", "desarrollo", "inteligencia"]
+
 # Elegir una palabra al azar
 secret_word = random.choice(words)
-# Número máximo de intentos permitidos
-max_attempts = 10
+
+# Contador de fallos
+cant_fallos = 0
+
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
+
 print("¡Bienvenido al juego de adivinanzas!")
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 word_displayed = "_" * len(secret_word)
+
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
-for i in range(max_attempts):
+
+while cant_fallos < 5:
     # Pedir al jugador que ingrese una letra
     letter = input('Ingresa una letra: ').lower()
     while letter == '':
@@ -29,6 +35,8 @@ for i in range(max_attempts):
         print("¡Bien hecho! La letra está en la palabra.")
     else:
         print("Lo siento, la letra no está en la palabra.")
+        cant_fallos += 1
+    print(f'Cantidad de fallos: {cant_fallos}\n')
     # Mostrar la palabra parcialmente adivinada
     letters = []
     for letter in secret_word:
@@ -42,6 +50,6 @@ for i in range(max_attempts):
     if word_displayed == secret_word:
         print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
         break
-    else:
-        print(f"¡Oh no! Has agotado tus {max_attempts} intentos.")
+    elif cant_fallos == 5:
+        print(f"¡Oh no! Has agotado tus intentos.")
         print(f"La palabra secreta era: {secret_word}")
